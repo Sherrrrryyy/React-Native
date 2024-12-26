f1a05aa067f1c439b3e08a34eefd46b83d8bccfb
Sherrrrryyy <Shaheerkhan090909@gmail.com>
2024-12-26 21:29:51 +0500

    React Native Learning

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,6 +7,8 @@ import { useEffect } from 'react';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
+import { View } from 'react-native';
+import { Text } from 'react-native';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -28,12 +30,10 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <View>
+<Text>
+  Hello React Native
+</Text>
+    </View>
   );
 }
