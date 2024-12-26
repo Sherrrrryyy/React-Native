6c4c3f03701e6a54135537435c1c8733995c6332
Sherrrrryyy <Shaheerkhan090909@gmail.com>
2024-12-26 21:35:20 +0500

    "Layout"

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -19,17 +19,7 @@ export default function RootLayout() {
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
 
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
-
-  return (
+    return (
     <View>
 <Text>
   Hello React Native
